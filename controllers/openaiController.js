0cf818bd87789edf455da0eca1d8b9878ad74825
Khan Mohd Akram Mohd Ajmal <111837120+252Akram@users.noreply.github.com>
2024-09-05 22:41:43 +0530

    Update openaiController.js

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -48,43 +48,5 @@ const generateImage = async(req, res) => {
     }
 };
 
-// HTML code for Text to Image Generator
-const htmlCode = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Text to Image Generator</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center my-4">Text to Image Generator</h1>
-        <form id="imageForm">
-            <div class="form-group">
-                <label for="prompt">Enter your prompt:</label>
-                <input type="text" class="form-control" id="prompt" required>
-            </div>
-            <div class="form-group">
-                <label for="size">Select image size:</label>
-                <select class="form-control" id="size">
-                    <option value="small">Small (256x256)</option>
-                    <option value="medium">Medium (512x512)</option>
-                    <option value="large">Large (1024x1024)</option>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary btn-block">Generate Image</button>
-        </form>
-        <div id="result" class="text-center my-4">
-            <img id="generatedImage" src="" alt="Generated Image" class="img-fluid d-none">
-        </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="script.js"></script>
-</body>
-</html>
-`;
 
 module.exports = { generateImage, htmlCode };
